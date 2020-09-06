ff9acbd944156e5d123e298e941422ab88aa6d6b
LeeEugene1 <uwm1004@gmail.com>
2020-09-06 17:57:52 +0900

    what's the deffrence between async and defer?

---
+++ 1.async&defer.js
@@ -0,0 +1,14 @@
+//added ES5, flexible === dangerous
+//자바스크립트 개발할때 꼭필요
+'use strict'
+
+a = 6//a is not defined라는 에러가뜨는데 만약 'use strict'가 없으면 에러안뜸
+
+console.log('Hello World')
+//node.js에는 자바스크립트 엔진이있어서 브라우저없이도 js파일 열수있음
+//vs code에는 consoleAPI 중 log함수를 이용해 콘솔창에 출력할수있다.
+
+//nodejs, vscode 둘다 콘솔 api가 있다.
+//Web API는 자바스크립트가아니라 브라우저가 이해할수있는 함수
+
+
